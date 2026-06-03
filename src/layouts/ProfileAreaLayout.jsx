9af49f696783da2e30f1a757acc0eb236eff0875
Luciano Gabutti <lgabutti10@gmail.com>
2026-06-03 19:14:21 -0300

    removed profile consts sx

--- src/layouts/ProfileAreaLayout.jsx
+++ src/layouts/ProfileAreaLayout.jsx
@@ -5,23 +5,10 @@ import { PageContainer } from '../components/layout/PageContainer';
 import { accountUser } from '../config/accountUser';
 import { profileSidebarItems } from '../config/sidebarItems';
 
-const layoutSx = {
-  display: 'flex',
-  alignItems: 'flex-start',
-  bgcolor: 'background.default',
-};
-
-const contentSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  minWidth: 0,
-};
-
 export const ProfileAreaLayout = () => (
-  <Box sx={layoutSx}>
+  <Box sx={{ display: 'flex', alignItems: 'flex-start', bgcolor: 'background.default' }}>
     <Sidebar user={accountUser} items={profileSidebarItems} />
-    <Box sx={contentSx}>
+    <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, minWidth: 0 }}>
       <PageContainer narrow py={6}>
         <Outlet />
       </PageContainer>
