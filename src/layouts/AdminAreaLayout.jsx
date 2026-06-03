648bb1328112d12b1afcc174524db1b0c0c049b0
Luciano Gabutti <lgabutti10@gmail.com>
2026-06-03 18:59:50 -0300

    removed sx consts

--- src/layouts/AdminAreaLayout.jsx
+++ src/layouts/AdminAreaLayout.jsx
@@ -7,24 +7,12 @@ import { accountUser } from '../config/accountUser';
 import { adminSidebarItems } from '../config/sidebarItems';
 import '../components/admin/styles.scss';
 
-const layoutSx = {
-  display: 'flex',
-  alignItems: 'flex-start',
-  bgcolor: 'background.default',
-};
-
-const contentSx = {
-  display: 'flex',
-  flexDirection: 'column',
-  flexGrow: 1,
-  minWidth: 0,
-};
 
 export const AdminAreaLayout = () => (
   <AdminProvider>
-    <Box sx={layoutSx}>
+    <Box sx={{ display: 'flex', alignItems: 'flex-start', bgcolor: 'background.default' }}>
       <Sidebar user={accountUser} items={adminSidebarItems} />
-      <Box sx={contentSx}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, minWidth: 0 }}>
         <PageContainer maxWidth={false} py={5}>
           <Box className="admin-main-inner">
             <Outlet />
