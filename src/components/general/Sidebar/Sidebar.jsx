4dfd471c9f4891e4b2e1181073d2b881cbb76579
Luciano Gabutti <lgabutti10@gmail.com>
2026-06-17 21:47:08 -0300

    profile page client data ok

--- src/components/general/Sidebar/Sidebar.jsx
+++ src/components/general/Sidebar/Sidebar.jsx
@@ -25,12 +25,12 @@ export const Sidebar = ({ user, items }) => {
   return (
     <Box component="aside" className="app-sidebar">
       <Box className="sidebar-user-summary">
-        <Avatar alt={user.name} src={user.avatarSrc} className="sidebar-user-avatar" />
+        <Avatar alt={user.nombreApellido} src='https://lh3.googleusercontent.com/aida-public/AB6AXuDJ5DcwCqUxBTP6jUksUtzDByChxWAMaRxgRb-6tvApdoUZOH8XOsp8n1PJCJJPuUZRTVECl-HhiENhrtjDAUxMjNT0dWk_jaVY7MMogUO2uNh44dDlShzZ5dU9bOstE25Yz8qlUvkuXqkmoDYH1pVxNmA_ymOybJ1GYUt_8jBKp83ivlgniU51oQJQo_Fan0Vj0NMubQdWnb8GZXB1aYfNY4l_RMJAOIC1KKhop46PZ9Yx8HcNbooFzLIjYmwoY5-o6xwXmHJ8DcQh' className="sidebar-user-avatar" />
         <Typography variant="h6" className="sidebar-user-name">
-          {user.name}
+          {user.nombreApellido}
         </Typography>
         <Typography variant="body2" className="sidebar-user-email">
-          {user.email}
+          {user.mail}
         </Typography>
       </Box>
 
